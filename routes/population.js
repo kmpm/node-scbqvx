6a0621d3d7065bd6ab502bddf8d6c9781304b28b
kmpm <peter@birchroad.net>
2016-03-30 17:21:57 +0000

    some meta to qvx

--- routes/population.js
+++ routes/population.js
@@ -1,16 +1,43 @@
 var express = require('express');
 var router = express.Router();
-var ScbClient = require('../lib/scbclient');
+var lib = require('../lib');
+var scb = new lib.ScbClient();
+var schemas = lib.schemas;
+//var debug = require('debug')('scbqvx:routes:population');
 
-var scb = new ScbClient();
+
+function sendQvx(res, data, schema) {
+  //debug('schema', schema);
+  res.set('Content-Type', 'application/octet-stream');
+  res.set('Content-Disposition', 'attachment; filename=' + schema.tableName + '.qvx;');
+  return lib.qvxify(schema, data).pipe(res);
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/meta', function(req, res, next) {
   scb.populationMeta()
   .then(function (data) {
-    res.json(data)
+    res.json(data);
   })
 });
+
+
+
+router.get('/meta/variables/:code', function(req, res, next) {
+  scb.populationMeta()
+  .then(function (data) {
+    return lib.filterByCode(req.params.code)(data.variables);
+  })
+  .then(function (data) {
+    var result = [];
+    for(var i = 0; i < data.values.length; i++) {
+      result.push({value: data.values[i], name: data.valueTexts[i]});
+    }
+    sendQvx(res, result, schemas.metaVariableValues);
+  })
+  .catch(next);
+});
+
 
 router.get('/meta/variables', function(req, res, next) {
   scb.populationMeta()
@@ -18,8 +45,12 @@ router.get('/meta/variables', function(req, res, next) {
     var result = data.variables.map(function(x) {
       return {code: x.code, text: x.text};
     });
-    res.json(result);
+    
+    sendQvx(res, result, schemas.metaVariables);
   })
+  .catch(next);
 });
+
+
 
 module.exports = router;
